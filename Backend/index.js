54fdc3a0332f50ae928c746c73f6b9e6e3b9d44a
Yossef Hernandez <yossef.her25@gmail.com>
2023-04-21 16:58:50 -0600

    1/2

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  //trycatch for create a user
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "4f15c2da-6eee-4e66-86c8-a193bd4f5716" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
