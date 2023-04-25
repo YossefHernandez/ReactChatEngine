edcf21afeb9a1118d28963dfd315bd852dce0a17
Yossef Hernandez <yossef.her25@gmail.com>
2023-04-25 12:03:06 -0600

    Finished

--- Frontend/src/ChatsPage.jsx
+++ Frontend/src/ChatsPage.jsx
@@ -1,5 +1,21 @@
-const ChatsPage = () =>{
-    return <>Chats...</>
-}
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
 
-export default ChatsPage
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "05a4c281-9177-4ea9-9f38-d0fef7c54f32",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+
+export default ChatsPage;
